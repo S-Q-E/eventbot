3cf1634b7fa30b1e12c12db8bf89c9f64633013e
S-Q-E <sagimbai.k@mail.ru>
2024-11-27 21:52:34 +0500

    add back button in user list function

--- handlers/user_list.py
+++ handlers/user_list.py
@@ -9,6 +9,8 @@ user_list_router = Router()
 @user_list_router.callback_query(F.data == "user_list")
 async def list_registered_users(callback: types.CallbackQuery):
     db = next(get_db())
+    back_btn = InlineKeyboardButton(text="Назад", callback_data="admin_panel")
+    markup = InlineKeyboardMarkup(inline_keyboard=[[back_btn]])
     try:
         # Запрашиваем всех зарегистрированных пользователей
         users = db.query(User).filter(User.is_registered == True).all()
@@ -18,10 +20,10 @@ async def list_registered_users(callback: types.CallbackQuery):
             return
 
         user_info = "\n".join([f"{user.first_name} {user.last_name} (@{user.username} {user.phone_number})" for user in users])
-        await callback.message.answer(text=f"Зарегистрированные пользователи:\n{user_info}")
+        await callback.message.answer(text=f"Зарегистрированные пользователи:\n🔹 {user_info}", reply_markup=markup)
 
     except Exception as e:
-        await callback.message.answer("Произошла ошибка при получении списка пользователей.")
+        await callback.message.answer("Произошла ошибка при получении списка пользователей.", reply_markup=markup)
         print(f"Ошибка при запросе пользователей: {e}")
     finally:
         db.close()
