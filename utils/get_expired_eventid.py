1c433cb83daa273986bfd66c633ab555c4bb1c69
S-Q-E <sagimbai.k@mail.ru>
2025-01-14 20:49:28 +0500

    commented feedback

--- utils/get_expired_eventid.py
+++ utils/get_expired_eventid.py
@@ -1,56 +1,56 @@
-from datetime import datetime
-from sqlalchemy.orm import Session, joinedload
-from db.database import Registration, User, Event
-
-
-def get_users_for_feedback(db: Session, event_id: int):
-    """
-    Получает список пользователей, которым нужно отправить запрос на отзыв.
-
-    Args:
-        db: Объект сессии базы данных SQLAlchemy.
-        event_id: ID события.
-
-    Returns:
-        Список пользователей, которым нужно отправить запрос.
-    """
-    registrations = (
-        db.query(Registration)
-        .filter(
-            Registration.event_id == event_id,
-            Registration.has_given_feedback == False  # Только пользователи без отзыва
-        )
-        .join(User, Registration.user_id == User.id)
-        .all()
-    )
-    return [reg.user for reg in registrations]
-
-
-def get_started_events(db: Session):
-    """
-    Получает начавшиеся события с пользователями, которым нужно отправить уведомление о предоставлении отзыва.
-
-    Args:
-        db: Объект сессии базы данных SQLAlchemy.
-
-    Returns:
-        Список словарей с id и именем начавшихся событий.
-    """
-    current_time = datetime.now()
-
-    events = (
-        db.query(Event)
-        .join(Registration, Event.id == Registration.event_id)  # Присоединяем регистрации
-        .filter(
-            Event.event_time <= current_time,                 # Событие уже началось
-            Registration.has_given_feedback == False          # Пользователь ещё не оставил отзыв
-        )
-        .options(joinedload(Event.registrations))             # Подгружаем регистрации
-        .distinct()                                           # Убираем дубли
-        .all()
-    )
-
-    started_events = [{"id": event.id, "name": event.name} for event in events]
-    return started_events
-
-
+# from datetime import datetime
+# from sqlalchemy.orm import Session, joinedload
+# from db.database import Registration, User, Event
+#
+#
+# def get_users_for_feedback(db: Session, event_id: int):
+#     """
+#     Получает список пользователей, которым нужно отправить запрос на отзыв.
+#
+#     Args:
+#         db: Объект сессии базы данных SQLAlchemy.
+#         event_id: ID события.
+#
+#     Returns:
+#         Список пользователей, которым нужно отправить запрос.
+#     """
+#     registrations = (
+#         db.query(Registration)
+#         .filter(
+#             Registration.event_id == event_id,
+#             Registration.has_given_feedback == False  # Только пользователи без отзыва
+#         )
+#         .join(User, Registration.user_id == User.id)
+#         .all()
+#     )
+#     return [reg.user for reg in registrations]
+#
+#
+# def get_started_events(db: Session):
+#     """
+#     Получает начавшиеся события с пользователями, которым нужно отправить уведомление о предоставлении отзыва.
+#
+#     Args:
+#         db: Объект сессии базы данных SQLAlchemy.
+#
+#     Returns:
+#         Список словарей с id и именем начавшихся событий.
+#     """
+#     current_time = datetime.now()
+#
+#     events = (
+#         db.query(Event)
+#         .join(Registration, Event.id == Registration.event_id)  # Присоединяем регистрации
+#         .filter(
+#             Event.event_time <= current_time,                 # Событие уже началось
+#             Registration.has_given_feedback == False          # Пользователь ещё не оставил отзыв
+#         )
+#         .options(joinedload(Event.registrations))             # Подгружаем регистрации
+#         .distinct()                                           # Убираем дубли
+#         .all()
+#     )
+#
+#     started_events = [{"id": event.id, "name": event.name} for event in events]
+#     return started_events
+#
+#
--- utils/scheduler_instance.py
+++ utils/scheduler_instance.py
@@ -1,37 +1,35 @@
 from aiogram import Bot
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from db.database import get_db, Event
-from utils.feedback_request import send_feedback_request
 from utils.notify_user import send_notifications
-from datetime import datetime, timedelta
+
 
 scheduler = AsyncIOScheduler()
 
-
-def check_events(bot: Bot, db_session: get_db):
-    """
-    Планирует отправку уведомлений для всех событий.
-
-    Args:
-        bot: Экземпляр бота Telegram.
-        db_session: Сессия базы данных.
-    """
-    with db_session as db:
-        events = db.query(Event).filter(Event.event_time >= datetime.now()).all()
-        for event in events:
-            # Если событие уже есть в планировщике, пропустим его
-            job_id = f"notify_event_{event.id}"
-            if scheduler.get_job(job_id):
-                continue
-
-            scheduler.add_job(
-                send_feedback_request,
-                trigger="date",
-                run_date=event.event_time + timedelta(minutes=120),
-                args=[bot, event.id, event.name],
-                id=job_id
-            )
-
+#
+# def check_events(bot: Bot, db_session: get_db):
+#     """
+#     Планирует отправку уведомлений для всех событий.
+#
+#     Args:
+#         bot: Экземпляр бота Telegram.
+#         db_session: Сессия базы данных.
+#     """
+#     with db_session as db:
+#         events = db.query(Event).filter(Event.event_time >= datetime.now()).all()
+#         for event in events:
+#             # Если событие уже есть в планировщике, пропустим его
+#             job_id = f"notify_event_{event.id}"
+#             if scheduler.get_job(job_id):
+#                 continue
+#
+#             scheduler.add_job(
+#                 send_feedback_request,
+#                 trigger="date",
+#                 run_date=event.event_time + timedelta(minutes=120),
+#                 args=[bot, event.id, event.name],
+#                 id=job_id
+#             )
+#
 
 # async def check_events(bot: Bot):
 #     """
@@ -48,10 +46,10 @@ def check_events(bot: Bot, db_session: get_db):
 #             db.close()
 
 
-def start_scheduler(bot):
+def start_scheduler(bot: Bot):
     """
     Запускает планировщик задач.
     """
     scheduler.add_job(send_notifications, 'interval', minutes=1, args=[bot])
-    scheduler.add_job(check_events, "interval", seconds=60, args=[bot, next(get_db())])  # Интервал: 1 минута
+    # scheduler.add_job(check_events, "interval", seconds=60, args=[bot, next(get_db())])  # Интервал: 1 минута
     scheduler.start()
