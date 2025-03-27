8c110eafbefa7ec0ad713048ae38425d7cc4a745
S-Q-E <sagimbai.k@mail.ru>
2025-03-27 10:14:10 +0500

    make easier add new user

--- handlers/manual_user_add.py
+++ handlers/manual_user_add.py
@@ -11,28 +11,26 @@ manual_add_user_router = Router()
 
 class AddManualUser(StatesGroup):
     wait_user_firstname = State()
-    wait_user_lastname = State()
     wait_user_phone = State()
 
 
 @manual_add_user_router.callback_query(F.data == "add_user")
 async def add_manual_user(callback: types.CallbackQuery, state: FSMContext):
-    await callback.message.answer("Введите имя нового пользователя: ")
+    await callback.message.answer("Введите имя и фамилию нового пользователя <b>через пробел</b>: ")
     await state.set_state(AddManualUser.wait_user_firstname)
 
 
 @manual_add_user_router.message(AddManualUser.wait_user_firstname)
 async def get_user_lastname(message: types.Message, state: FSMContext):
-    await state.update_data(first_name=message.text)
-    await message.answer("Введите фамилию нового пользователя")
-    await state.set_state(AddManualUser.wait_user_lastname)
-
-
-@manual_add_user_router.message(AddManualUser.wait_user_lastname)
-async def get_user_lastname(message: types.Message, state: FSMContext):
-    await state.update_data(last_name=message.text)
-    await message.answer("Введите номер телефона нового пользователя")
-    await state.set_state(AddManualUser.wait_user_phone)
+    try:
+        first_name, last_name = message.text.split(" ")
+        await state.update_data(first_name=first_name)
+        await state.update_data(last_name_name=last_name)
+        await message.answer("Теперь введите номер телефона")
+        await state.set_state(AddManualUser.wait_user_phone)
+    except IndexError as e:
+        await message.answer("Проверьте правильность введения данных. Нужно вводить через пробел")
+        return
 
 
 @manual_add_user_router.message(AddManualUser.wait_user_phone)
@@ -42,7 +40,6 @@ async def get_user_phone(message: types.Message, state: FSMContext):
     last_name = data.get("last_name")
     phone = message.text
     with next(get_db()) as db:
-        db = next(get_db())
 
         new_id = generate_unique_id_with_uuid(db)
         if not phone.startswith("+7"):
